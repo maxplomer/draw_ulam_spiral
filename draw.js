a61c763eefb44ad098c3a00a0890888ccdf28315
Max Plomer <maxplomer@gmail.com>
2025-01-19 11:22:10 -0500

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,10 +1,17 @@
 const { createCanvas, loadImage } = require('canvas')
+const terminalImagePromise = import('terminal-image')
 const canvas = createCanvas(200, 200)
 const ctx = canvas.getContext('2d')
 
 ctx.fillRect(0, 0, 1, 1);
 ctx.fillRect(100, 100, 1, 1);
 
-dataUrl = canvas.toDataURL()
+const base64String = canvas.toDataURL()
+const base64Data = base64String.replace(/^data:image\/\w+;base64,/, "");
+const buffer = Buffer.from(base64Data, "base64");
 
-console.log(dataUrl)
+terminalImagePromise.then((terminalImage) => {
+  terminalImage.default.buffer(buffer).then((image) => {
+    console.log(image)
+  })
+});
