0f4db41b92e8923f4f10b2fb3e3483a9a72154cc
Max Plomer <maxplomer@gmail.com>
2025-01-19 10:52:28 -0500

    Update draw.js

--- draw.js
+++ draw.js
@@ -2,10 +2,8 @@ const { createCanvas, loadImage } = require('canvas')
 const canvas = createCanvas(200, 200)
 const ctx = canvas.getContext('2d')
 
-// Write "Awesome!"
-ctx.font = '30px Impact'
-ctx.rotate(0.1)
-ctx.fillText('Awesome!', 50, 100)
+ctx.fillRect(0, 0, 1, 1);
+ctx.fillRect(100, 100, 1, 1);
 
 dataUrl = canvas.toDataURL()
 
